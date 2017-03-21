e1219829b2265d28ac72658db0b29a049217b56a
Mathias Rav <rav@cs.au.dk>
2017-03-21 16:48:12 -0400

    Run dmarc_policy_parser.public_suffix.test in dmarc_policy_parser/__main__.py

--- dmarc_policy_parser/__main__.py
+++ dmarc_policy_parser/__main__.py
@@ -1,5 +1,6 @@
 import logging
 from dmarc_policy_parser import get_dmarc_policy
+from dmarc_policy_parser.public_suffix import test as test_public_suffix
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -11,3 +12,5 @@ for d in 'gmail.com hotmail.com google.com yahoo.com tdc.dk'.split():
         print(get_dmarc_policy(d))
     except Exception as exn:
         print(repr(exn))
+
+test_public_suffix()
